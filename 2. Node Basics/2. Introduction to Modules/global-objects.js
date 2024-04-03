214cd1dcda3313922a02aeed71da343975d77aae
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-03 14:35:53 +0500

    Global Objects

---
+++ 2. Node Basics/2. Introduction to Modules/global-objects.js
@@ -0,0 +1,15 @@
+// Global Objects
+
+// "global" object is global scope so we don't need
+// to import using the require() statement
+
+// Web browser -> "window" object
+// Node -> "global" object
+
+console.log(global);
+
+console.log(module);
+
+// the "__filename" gives the current path & file name.
+console.log(__filename);
+console.log(__dirname);
