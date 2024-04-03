fd24e8ecb4168658fb41840e1b1de379c2dd3131
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-03 16:43:07 +0500

    clearTimeout Global object Method

--- 2. Node Basics/2. Introduction to Modules/setTimeout-method.js
+++ 2. Node Basics/2. Introduction to Modules/setTimeout-method.js
@@ -16,3 +16,25 @@ console.log('Timer Started.');
 
 //  To stop the setTimeout()'s timer, we have another method
 // called "clearTimeout()".
+
+// The "clearTimeout()" method is used to stop the timer set
+// using setTimeout() method.
+
+// const setTimeoutInstance = setTimeout(function, time)
+// clearTimeout(setTimeoutInstance)
+
+secondTimer = setTimeout(() => {
+  console.log('Timed Out!');
+}, 3000);
+
+const stopTimer = () => {
+  clearTimeout(secondTimer);
+  console.log('Timer Cleared!');
+};
+
+console.log('Timer started');
+
+stopTimer();
+
+// To repeat the process on regular intervals of time,
+// we have the "setInterval()" method.
