c72e1743e07fa0586fe50dec918f5f48bd8e148c
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-19 09:30:52 +0500

    Auto Restart Server Nodemon

--- 5.HTTP_Module/server.js
+++ 5.HTTP_Module/server.js
@@ -14,3 +14,7 @@ const server = http.createServer((req, res) => {
   console.log('Server running...');
 });
 server.listen(3000);
+
+// Server needs to be stoped & restarted everytime after doing the changes in the code.
+// A utility is needed which can keep a watch on changes & restart the server automatically.
+// "nodemon" is the utility which monitors the changes & restarts the server.
