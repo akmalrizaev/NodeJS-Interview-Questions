9313f90fdd6c39b2a05d2ea98fb7407321b46261
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-20 16:44:53 +0500

    Sending JSON Response

--- 5.HTTP_Module/routes.js
+++ 5.HTTP_Module/routes.js
@@ -2,6 +2,11 @@
 
 const http = require('http');
 
+const product = {
+  pcode: 1001,
+  product: 'Apple',
+};
+
 const server = http.createServer((req, res) => {
   let url = req.url;
   if (url === '/') {
@@ -12,11 +17,8 @@ const server = http.createServer((req, res) => {
     res.write('</body></html>');
     return res.end(); // Must use return with res.end() when there are conditional responses for different urls.
   } else if (url === '/data') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title>JSON Data</title></head>');
-    res.write('<body><h1>JSON Data</h1>');
-    res.write('<h3>' + req.url + '</h3>');
-    res.write('</body></html>');
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(product));
     res.end();
   }
 });
