f7403d904214208ef3be6e4b373170bd7f030f23
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-22 10:59:07 +0500

    Headers JSON Response

--- 6.Expressjs/respond.js
+++ 6.Expressjs/respond.js
@@ -4,14 +4,22 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+const data = {
+  pCode: 1001,
+  pName: 'Apple',
+  price: 20,
+};
 
 app.get('/', (req, res) => {
   //   res.send('Hello World');
   //   res.send('<h1>Hello World!</h1>');
-  res.write('<html><head><title> Node Server </title></head>');
-  res.write('<body> <h1> Responding to the Request </h1>');
-  res.write('<h1> URL : ' + req.url + '</h1>');
-  res.write('</body></html>');
+  //   res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'application/json');
+  //   res.write('<html><head><title> Node Server </title></head>');
+  //   res.write('<body> <h1> Responding to the Request </h1>');
+  //   res.write('<h1> URL : ' + req.url + '</h1>');
+  //   res.write('</body></html>');
+  res.write(JSON.stringify(data));
   res.end();
 });
 
