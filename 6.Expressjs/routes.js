66ab685982d8e6b56dd7ea88a393a0fb384884d6
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-23 17:34:52 +0500

    Wildcard Characters in Routing

--- 6.Expressjs/routes.js
+++ 6.Expressjs/routes.js
@@ -17,6 +17,25 @@ app.get('/products', (req, res) => {
   res.end();
 });
 
+// Wildcard characters enable various pattern matchings in the routes
+// ? for optional character
+// + for n-number of same characters
+// * for any string
+
+app.get('/product-01', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1> Welcome to Products Route </h1>');
+  res.write('<h1> URL : ' + req.url + '</h1>');
+  res.end();
+});
+
+app.get('/product*-01', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1> Welcome to Products Route </h1>');
+  res.write('<h1> URL : ' + req.url + '</h1>');
+  res.end();
+});
+
 app.listen(PORT, () => {
   console.log('Server is Running');
 });
