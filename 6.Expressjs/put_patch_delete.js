f807c780f180230e5fd3828b8b5311a4ed7edc3c
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-24 08:47:59 +0500

    PUT PATCH DELETE Request

---
+++ 6.Expressjs/put_patch_delete.js
@@ -0,0 +1,5 @@
+// Apart from GET & POST requests, there are other types of requests too.
+// There are 3 more requests which are important:
+// PUT request - To edit/modify. Overwrites the data
+// PATCH request - To edit/modify. Updates a specific field
+// DELETE request - To delete
