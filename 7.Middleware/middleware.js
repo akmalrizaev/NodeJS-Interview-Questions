691bba46a0927aad487899d73023a94b4b92a3d2
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-25 10:03:39 +0500

    Inroduction to Middleware

---
+++ 7.Middleware/middleware.js
@@ -0,0 +1,8 @@
+// Middleware is a function receiving req & res object
+// & is called automatically between request & response cycle.
+
+// If the middleware does not end the response cycle, then we must call
+// the next() function to pass the control to the next middleware function.
+
+// Application level middleware
+// Route level middleware
