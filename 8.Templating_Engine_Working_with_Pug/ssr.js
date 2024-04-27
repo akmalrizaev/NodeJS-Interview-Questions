5993cd759db3fa8d1f1f560e9a9a65d91a424100
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-27 09:53:23 +0500

    Introduction to Server Side Rendering

---
+++ 8.Templating_Engine_Working_with_Pug/ssr.js
@@ -0,0 +1,17 @@
+// SSR - Server Side Rendering
+// We need "Templating Engine" to help in preparing
+// the client side page on the server.
+// - Faster rendering of webpage
+// - Optimazed choice for SEO
+
+// CSR - Client Side Rendering
+// - Dynamic & interactive website
+// - Smoother user experience
+// - Server requests frequency
+// - Not as SEO friendly as SSR
+
+// Using a templating engine provides various features for making the code dynamic
+// like - variables values, loops, conditions, etc., with HTML code on the server itself.
+
+// We can also have layouts which are similar to components
+// in frameworks like Angular, React etc.
