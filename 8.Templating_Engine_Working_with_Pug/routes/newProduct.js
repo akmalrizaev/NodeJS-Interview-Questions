e4e9475a042211bf5c45a7973b5c3636dd4bfd7e
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-04-29 08:40:57 +0500

    Fetching the Product Data

--- 8.Templating_Engine_Working_with_Pug/routes/newProduct.js
+++ 8.Templating_Engine_Working_with_Pug/routes/newProduct.js
@@ -2,9 +2,20 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const dirPath = require('../utils/path');
+const bodyParser = require('body-parser');
 
-router.get('/',(req,res)=>{
+const products = [];
+
+router.use(bodyParser.urlencoded());
+
+router.get('/', (req, res) => {
   res.render('new-product');
-})
+});
+
+router.post('/', (req, res) => {
+  products.push({ title: req.body.title, img: req.body.img });
+  console.log(products);
+  res.redirect('/');
+});
 
 exports.route = router;
