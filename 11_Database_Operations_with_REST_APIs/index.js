c460f3d74a694a68489d1665b9b934cbee4e6666
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-14 18:33:13 +0500

    Updating database with PUT

--- 11_Database_Operations_with_REST_APIs/index.js
+++ 11_Database_Operations_with_REST_APIs/index.js
@@ -56,6 +56,22 @@ app.delete('/deleteData', (req, res) => {
   });
 });
 
+// Updating database with PUT
+app.put('/putData', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.send(err.sqlMessage);
+    } else {
+      pool.execute(
+        'update products set productname="Pineapple" price=20 where id=1',
+        (error, rshObj) => {
+          res.send(rshObj).status(200);
+        }
+      );
+    }
+  });
+});
+
 const server = app.listen(3000, () => {
   console.log('Server is Running...');
 });
@@ -66,3 +82,12 @@ const server = app.listen(3000, () => {
 // - Code(300-399) : Redirection messages
 // - Code(400-499) : Client error responses
 // - Code(500-599) : Server error responses
+
+// What are PUT & PATCH requests?
+// Both are updating type of requests.
+// When making a query to update the database in any sense, we use either of
+// these two methods.
+// PUT is used when an entire dataset, e.g a record is to be updated at once
+// PUT will overwrite the entire record
+// PATCH request will make a patch & edit the specific field value only.
+// PATCH wil overwrite only the specific fields.
