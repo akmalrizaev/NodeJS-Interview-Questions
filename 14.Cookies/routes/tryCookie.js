a8727c55e68f20191e3de23b955c78b9478fd8f3
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-28 12:33:41 +0500

    Expiring Cookie - expires & maxAge attributes

--- 14.Cookies/routes/tryCookie.js
+++ 14.Cookies/routes/tryCookie.js
@@ -10,8 +10,17 @@ router.get('/', (req, res) => {
 
   //   res.send('Cookie Sent');
 
-  const cookie = req.get('Cookie');
-  console.log(cookie);
+  //   const cookie = req.get('Cookie');
+  //   console.log(cookie);
+
+  //   "expires" & "max-age" are used to control the lifespan/age of a cookie
+
+  const expDate = new Date(Date.now() + 5e3).toUTCString();
+  //   res.cookie("isLoggedIn","true", {expires:expDate});
+  //   res.setHeader('Set-Cookie', 'isLoggedIn=true; max-age=5');
+  //   res.cookie('isLoggedIn', 'true', { maxAge: 5000 });
+  res.setHeader('Set-Cookie', 'isLoggedIn=true; expires=' + expDate);
+  res.send(expDate);
 });
 
 router.post('/', (req, res) => {
