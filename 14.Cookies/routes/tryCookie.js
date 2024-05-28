78e49699c78b3b42afa5c4b975f88eb81fbc61f6
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-28 10:37:44 +0500

    Reading a Cookie

--- 14.Cookies/routes/tryCookie.js
+++ 14.Cookies/routes/tryCookie.js
@@ -6,9 +6,21 @@ router.get('/', (req, res) => {
 
   //   res.setHeader('Set-Cookie', 'isLoggedIn=true');
 
-  res.cookie('isLoggedIn', 'true');
+  //   res.cookie('isLoggedIn', 'true');
 
-  res.send('Cookie Sent');
+  //   res.send('Cookie Sent');
+
+  const cookie = req.get('Cookie');
+  console.log(cookie);
 });
+
+router.post('/', (req, res) => {
+  res.cookie('isLoggedIn', 'true');
+  res.send('Cookie Sent.');
+});
+
+// For all the subsequent requests - the same cookie will be sent
+// to the server for all requests.
+// And the sent cookie should also be read on the server.
 
 module.exports = router;
