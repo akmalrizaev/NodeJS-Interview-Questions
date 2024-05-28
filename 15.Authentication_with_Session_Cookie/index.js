f9e4d724f6724566da4f8ce401f7766dd8ff1425
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-28 16:48:11 +0500

    Authentication with Session Cookie

---
+++ 15.Authentication_with_Session_Cookie/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const home = require('./routes/home');
+const addProduct = require('./routes/addProduct');
+const editProduct = require('./routes/editProduct');
+const deleteProduct = require('./routes/deleteProduct');
+const tryCookie = require('./routes/tryCookie');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use('/', home);
+app.use('/add-product', addProduct);
+app.use('/edit-product', editProduct);
+app.use('/delete-product', deleteProduct);
+app.use('/tryCookie', tryCookie);
+
+app.use(express.static(__dirname));
+
+const server = app.listen(3000, () => {
+  console.log('Server Running...');
+});
