7ecac6265093a2c00588a4732a2f6b28b7cca1f3
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-05-28 15:09:04 +0500

    Optional secure attributes of cookie

--- 14.Cookies/routes/tryCookie.js
+++ 14.Cookies/routes/tryCookie.js
@@ -19,8 +19,30 @@ router.get('/', (req, res) => {
   //   res.cookie("isLoggedIn","true", {expires:expDate});
   //   res.setHeader('Set-Cookie', 'isLoggedIn=true; max-age=5');
   //   res.cookie('isLoggedIn', 'true', { maxAge: 5000 });
-  res.setHeader('Set-Cookie', 'isLoggedIn=true; expires=' + expDate);
-  res.send(expDate);
+  //   res.setHeader('Set-Cookie', 'isLoggedIn=true; expires=' + expDate);
+  //   res.send(expDate);
+
+  //   The cookies are not protected on the browser.
+  // They can be accessed from the browser.
+  // The client side Javascript code can tamper with the cookies.
+
+  // To prevent Javascript from accessing the cookie,
+  // we can set the cookie as "httpOnly".
+
+  res.cookie('isLoggedIn', 'true', {
+    expires: expDate,
+    httpOnly: true,
+  });
+
+  res.send('Cookie Sent.');
+
+  //   How can we have another level of security where only the URLs
+  // with "https" should be able to access the cookie?
+  // using "secure" atribute
+
+  //   "sameSite" attribute is especially useful for protection against
+  // XSRF type of attacks
+  //   sameSite: "strict"
 });
 
 router.post('/', (req, res) => {
