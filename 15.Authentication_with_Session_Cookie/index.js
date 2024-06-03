adcfce2f02c026a0b8646f1d1c9b681e4ce05820
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-06-03 18:21:54 +0500

    Storing session in MySQL

--- 15.Authentication_with_Session_Cookie/index.js
+++ 15.Authentication_with_Session_Cookie/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const MysqlStore = require('express-mysql-session')(session);
 
 const home = require('./routes/home');
 const addProduct = require('./routes/addProduct');
@@ -8,6 +9,18 @@ const editProduct = require('./routes/editProduct');
 const deleteProduct = require('./routes/deleteProduct');
 const tryCookie = require('./routes/tryCookie');
 const userAuth = require('./routes/userAuth');
+
+const options = {
+  connectionLimit: 10,
+  port: 3306,
+  host: 'localhost',
+  database: 'mystore',
+  user: 'root',
+  password: 'akmal',
+  createDatabaseTable: true,
+};
+
+const sessionStore = new MysqlStore(options);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -17,6 +30,7 @@ app.use(
     secret: 'It is a secret',
     resave: false,
     saveUninitialized: false,
+    store: sessionStore,
   })
 );
 
