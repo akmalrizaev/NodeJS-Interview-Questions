2f51651080c6cd2643bb70baed733ad09f887717
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-06-22 11:03:37 +0500

    New project for File Uploading/Downloading with Multer

---
+++ 18.File_UploadDownload_with_Multer/controllers/userAuthController.js
@@ -0,0 +1,65 @@
+const Users = require('../models/users');
+const JWT = require('jsonwebtoken');
+const { tokenSignature } = require('../utils/globals');
+const bcrypt = require('bcrypt');
+
+exports.renderSignUp = (req, res) => {
+  // const cookie = req.cookies;
+  const cookie = req.session.isLoggedIn;
+
+  res.render('sign-up', { isLoggedIn: global.isLoggedIn });
+};
+
+exports.registerUser = async (req, res) => {
+  const { userName, password, confirmPassword } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const users = new Users(null, userName, hashedPassword);
+
+  users.insertUser().then(() => {
+    res.redirect('/');
+  });
+};
+
+exports.renderLogin = (req, res) => {
+  // const cookie = req.cookies;
+  const cookie = req.session.isLoggedIn;
+
+  // res.render('login', { isLoggedIn: cookie.isLoggedIn });
+  res.render('login', { isLoggedIn: global.isLoggedIn });
+};
+
+exports.validateLogin = (req, res) => {
+  const { userName, password } = req.body;
+
+  Users.fetchUserByUsername(userName).then(([[userCredentials], tInfo]) => {
+    if (userCredentials) {
+      const isMatch = bcrypt.compare(userCredentials.password, password);
+
+      if (isMatch) {
+        // res.cookie('isLoggedIn', 'true');
+        const token = JWT.sign({ userName }, tokenSignature);
+        // req.session.isLoggedIn = 'true';
+        req.session.token = token;
+        res.redirect('/');
+      } else {
+        // res.cookie('isLoggedIn', 'invalidPassword');
+        // req.session.isLoggedIn = 'invalidPassword';
+        res.redirect('/login');
+      }
+    } else {
+      // res.cookie('isLoggedIn', 'invalidUserName');
+      // req.session.isLoggedIn = 'invalidUsername';
+      res.redirect('/login');
+    }
+  });
+};
+
+exports.logout = (req, res) => {
+  // res.cookie('isLoggedIn', 'false');
+  // req.session.isLoggedIn = 'false';
+
+  req.session.destroy(req.session.id);
+  res.redirect('/');
+};
---
+++ 18.File_UploadDownload_with_Multer/utils/globals.js
@@ -0,0 +1,1 @@
+exports.tokenSignature = 'it is a secret';
