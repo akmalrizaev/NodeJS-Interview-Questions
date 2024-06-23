6bfa9aa877b0be704f09bbad92985db49f6ceff1
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-06-23 16:06:27 +0500

    Creating Model with Sequelize

--- 19.Sequelize/server/index.js
+++ 19.Sequelize/server/index.js
@@ -1,17 +1,23 @@
 // import { Sequelize } from 'sequelize';
 
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('test', 'root', 'akmal', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: 3306,
-});
+// const sequelize = new Sequelize('test', 'root', 'akmal', {
+//   dialect: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+// });
+
+const { sequelize } = require('./db.js');
+const Product = require('./models/product');
 
 async function main() {
   try {
     await sequelize.authenticate();
     console.log('Connection established');
+
+    await Product.sync();
+    console.log('Table created');
   } catch (error) {
     console.error(error);
   } finally {
---
+++ 19.Sequelize/server/models/product.js
@@ -0,0 +1,34 @@
+// Creating Model with Sequelize
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+// In sequelize, the term table is referred to as "models"
+
+// With "models" we can define several things to Sequelize like
+//  - Name of the table
+//  - Column details
+//  - Data types
+
+const Product = sequelize.define('product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+  },
+  category: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Product;
+
+// How we are going to synchronize this model with the database?
+// Sequelize provides a method called sync() i.e, synchranization to synchronize
+// the model with database
