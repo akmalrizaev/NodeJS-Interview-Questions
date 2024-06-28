9268053b073f5c43e4f534e63eae4f3f36e3ef9d
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-06-28 10:18:13 +0500

    Inserting document

--- 21.Node_MongoDB_Basics/index.js
+++ 21.Node_MongoDB_Basics/index.js
@@ -1,6 +1,6 @@
 //import { MongoClient } from 'mongodb';    // ESM
 const { MongoClient } = require('mongodb'); // CJS
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(url);
 
 const dbName = 'courses';
@@ -10,6 +10,25 @@ async function main() {
   console.log('Connection successfully to server');
   const db = client.db(dbName);
   const collection = db.collection('topics');
+  //   const insertData = await collection.insertOne({
+  //     title: 'Frontend',
+  //     technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
+  //   });
+  const insertData = await collection.insertMany([
+    {
+      title: 'Frontend',
+      technologies: ['HTML', 'CSS', 'JavaScript', 'React'],
+    },
+    {
+      title: 'Backend',
+      technologies: ['Node.js', 'Python', 'MongoDB'],
+    },
+  ]);
+  if (insertData) {
+    console.log('Document Inserted');
+  } else {
+    throw new Error('Something went wrong');
+  }
   return 'done';
 }
 
